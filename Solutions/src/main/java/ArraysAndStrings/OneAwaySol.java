927a0507d566c033dc67ad00d78e261d7cd09eef
Benjamin <omben001@cougars.csusm.edu>
2020-08-23 22:30:24 -0700

    One Away solved

---
+++ Solutions/src/main/java/ArraysAndStrings/OneAwaySol.java
@@ -0,0 +1,24 @@
+package ArraysAndStrings;
+
+/**
+ * There are three types of operations that can be done on a string
+ * 1. insert a character
+ * 2. remove a character
+ * 3. replace a character
+ *
+ * Given two strings, write a function that determines whether the they are one edit or aways
+ *
+ * Return
+ * "one" if they are one edit away
+ * "zero" if they are the same
+ * "void" if they are more that one edits away
+ */
+
+
+public class OneAwaySol {
+
+    public static String OneAway(String stringA, String stringB){
+
+        return "void";
+    }
+}
